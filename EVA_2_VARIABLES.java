c557561574f4d31ffe801092aa0279f9b0c1ab47
dana-meraz <L24550372@chihuahua2.tecnm.mx>
2024-08-27 15:57:17 -0600

    Add files via upload

---
+++ EVA_2_VARIABLES.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");//Cadena de texto
+        System.out.println(100);//un numero entero
+        System.out.println('c');//un caracter
+        System.out.println(true);  //valor logico
+        System.out.println(false);
+        System.out.println(1000.52);  //número con fracción (con numereros de cimales)
+        
+        //Tipos de datos
+        //Enteros
+        //int
+        //long
+        //byte
+        //short
+        //Puntos frotantes
+        //double
+        //float
+        //Texto
+        //string
+        //Caracter
+        //char
+        //Lógicos (true false)
+        //boolear
+        
+        //creacion y uso de variables
+        int edad; //declaro una variable de tipo entero, llamada
+        //edad
+        //double edad; //NO PODEMOS TENER UNA VARIABLE CON EL MISMO NOMBRE
+        String nombre;
+        String apellido;
+        double promedio;
+        
+        edad = 18; // estoy asignando el valor 18 dentrode la variable edad
+        
+        System.out.println(edad); //println tambien variables 
+        
+        
+    }
+    
+}
