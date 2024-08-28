7dc07503544fc4db7c41441fe623341798e89b4a
dana-meraz <L24550372@chihuahua2.tecnm.mx>
2024-08-28 16:05:32 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_3_automovil;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //DECLARACIÓN DE LAS VARIABLES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "Ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+        
+        System.out.println(marca);//Iprime el contenido de la variable 
+        System.out.println(modelo);
+         System.out.println(year);
+          System.out.println(precio);
+    }
+    
+}
---
+++ EVA1_4_TELEVICIÓN.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_televición;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_TELEVICIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        double precio;
+        int pulgadas;
+        
+        marca = "Samsung";
+        precio = 1999.00;
+        pulgadas = 34;
+        
+        System.out.println(marca);
+        System.out.println(precio);
+        System.out.println(pulgadas);
+        
+    }
+    
+}
